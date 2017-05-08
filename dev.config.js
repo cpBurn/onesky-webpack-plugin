85a72f0e236edcfd5e6c4e4fec154c8e3b0986a8
Daniel Capeletti <daniel.capeletti@unit9.com>
2017-05-08 18:17:53 +0200

    fix webpack to ignore node_modules

--- dev.config.js
+++ dev.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: './src/index.js',
     target: 'node',
+    externals: [nodeExternals()],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, "dist"),
@@ -12,8 +14,8 @@ module.exports = {
     module: {
         loaders: [{
             test: /\.js$/,
-            exclude: /node_modules/,
             loader: 'babel-loader',
+            exclude: /node_modules/,
             query: {
                 presets: ['es2015']
             }
